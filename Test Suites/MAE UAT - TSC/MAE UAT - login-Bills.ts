8bdd3bf3bd0c7bcaf94c32c0b97344637d2fb665
Sam SIT Tester (BAU) <mohd.aisamudin@maybank.com>
2023-04-17 14:11:39 +0800

    test suite

---
+++ Test Suites/MAE UAT - TSC/MAE UAT - login-Bills.ts
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteCollectionEntity>
+   <description></description>
+   <name>MAE UAT - login-Bills</name>
+   <tag></tag>
+   <delayBetweenInstances>0</delayBetweenInstances>
+   <executionMode>SEQUENTIAL</executionMode>
+   <maxConcurrentInstances>8</maxConcurrentInstances>
+   <testSuiteRunConfigurations/>
+</TestSuiteCollectionEntity>
